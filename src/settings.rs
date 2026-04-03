2762b2ac9052e33b30282fb129582ea6f1c95b5a
Bea Boxler <bea@boxler.me>
2026-04-03 23:22:22 +0100

    fix settings path

--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,10 @@
-use std::fs::{read_to_string, write};
-
+use anyhow::Result;
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::{
+    fs::{create_dir_all, read_to_string, write},
+    path::PathBuf,
+};
 
 const SETTINGS_PATH: &str = "settings.json";
 
@@ -25,16 +28,30 @@ pub fn settings_plugin(app: &mut App) {
 
 fn save_settings(_commands: Commands, settings: Res<Settings>) {
     let serialized_settings = serde_json::to_string(&settings.into_inner()).unwrap();
-
-    write("settings.json", &serialized_settings).expect("Error, could not write settings");
+    let settings_path = get_settings_dir();
+    create_dir_all(&settings_path).ok();
+    write(
+        append_settings_filename(settings_path),
+        &serialized_settings,
+    )
+    .expect("Error, could not write settings");
 }
 
 fn load_settings() -> Result<Settings> {
-    match read_to_string(SETTINGS_PATH) {
-        Err(e) => Result::Err(e.into()),
-        Ok(settings) => match serde_json::from_str::<Settings>(&settings) {
-            Err(e) => Result::Err(e.into()),
-            Ok(settings) => Ok(settings),
-        },
-    }
+    let settings_string = read_to_string(append_settings_filename(get_settings_dir()))?;
+
+    Ok(serde_json::from_str(&settings_string)?)
+}
+
+fn get_settings_dir() -> PathBuf {
+    let mut path = std::env::home_dir().unwrap();
+    path.push(".config/pong");
+
+    path
+}
+
+fn append_settings_filename(mut path: PathBuf) -> PathBuf {
+    path.push(SETTINGS_PATH);
+
+    path
 }
